7a8eb062b50cb1c45059214a2586d809066c77fa
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2021-07-24 18:18:25 +0900

    simple server

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
+use actix_web::middleware::Logger;
+use actix_web::{get, App, HttpServer, Responder};
+use anyhow::bail;
 use anyhow::{Context, Result};
-use std::env;
 
-fn parse_args(args: Vec<String>) -> Result<(i128, i128)> {
-    let init: &i128 = &args[1].parse().context("cant parse arg[1]!")?;
-    let times: &i128 = &args[2].parse().context("cant parse arg[2]!")?;
-    Ok((*init, *times))
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello World"
 }
 
-use anyhow::bail;
-fn main() -> Result<(), anyhow::Error> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        bail!("Two numeric args are required!");
-    };
-
-    let (init, times) = parse_args(args)?;
-
-    let mut result = init;
-    for _time in 1..times {
-        result = result * 2;
-    }
-    println!("{}", result);
-    Ok(())
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    env_logger::init();
+    HttpServer::new(|| App::new().wrap(Logger::default()).service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
