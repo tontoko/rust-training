5d77657efd1f2e5d76096baaf70000bdc6f8e59b
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2021-07-24 20:03:18 +0900

    show html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+
+use actix_files::NamedFile;
 use actix_web::middleware::Logger;
 use actix_web::{get, App, HttpRequest, HttpServer, Responder};
 use anyhow::bail;
@@ -7,6 +10,13 @@ use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 #[get("/")]
 async fn index(_req: HttpRequest) -> impl Responder {
     "Hello World"
+}
+
+#[get("/html")]
+async fn html(req: HttpRequest) -> impl Responder {
+    // "html!!"
+    let path = "public/index.html";
+    NamedFile::open(path)
 }
 
 #[actix_web::main]
@@ -19,8 +29,13 @@ async fn main() -> Result<(), std::io::Error> {
         .unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
 
-    HttpServer::new(|| App::new().wrap(Logger::default()).service(index))
-        .bind_openssl("127.0.0.1:8443", builder)?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(index)
+            .service(html)
+    })
+    .bind_openssl("127.0.0.1:8443", builder)?
+    .run()
+    .await
 }
