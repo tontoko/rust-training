c72ea5ee08690daeb139ee804b3b3fa3d8af0ed9
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2021-07-24 19:40:18 +0900

    use ssl with local ca

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use actix_web::middleware::Logger;
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
 use anyhow::bail;
 use anyhow::{Context, Result};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[get("/")]
-async fn index() -> impl Responder {
+async fn index(_req: HttpRequest) -> impl Responder {
     "Hello World"
 }
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     env_logger::init();
+
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+
     HttpServer::new(|| App::new().wrap(Logger::default()).service(index))
-        .bind("127.0.0.1:8080")?
+        .bind_openssl("127.0.0.1:8443", builder)?
         .run()
         .await
 }
